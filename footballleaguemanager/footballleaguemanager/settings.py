bcc010b89b0b736d68b8a2b0a4e0cc6f86f6e148
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2025-05-24 18:29:05 -0300

    feat(postgresql): connect to postgresql

--- footballleaguemanager/footballleaguemanager/settings.py
+++ footballleaguemanager/footballleaguemanager/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'footballleaguemanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'footballleaguemanager',
+        'USER': 'adm',
+        'PASSWORD': 'adm',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
