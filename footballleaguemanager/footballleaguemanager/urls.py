76d0c5bad8dc0a959d572c2398b9d27ee73fe5a0
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2025-05-24 19:26:16 -0300

    feat(main): create the project's main page.

--- footballleaguemanager/footballleaguemanager/urls.py
+++ footballleaguemanager/footballleaguemanager/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from leagues.views import home
+from main.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home),    
+    path('', home),
 ]
