552150ec5904b831ccc8cade972f5c1481bc4473
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2025-05-25 11:17:10 -0300

    feat(leagues): create league model.

---
+++ footballleaguemanager/main/enums.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class LeagueType(Enum):
+    POINTS_BASED = 1, "Pontos corridos"
+    KNOCKOUT_CUP = 2, "Copa"
--- footballleaguemanager/main/models.py
+++ footballleaguemanager/main/models.py
@@ -1,3 +1,16 @@
+import uuid
 from django.db import models
+from enums import LeagueType;
 
-# Create your models here.
+class League(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    created_at = models.DateField()
+    type = models.IntegerField(
+        choices=LeagueType.choices,
+        default=LeagueType.POINTS_BASED
+        )
+    has_group_stage = models.BooleanField()
+    
+    def __str__(self):
+        return self.name
