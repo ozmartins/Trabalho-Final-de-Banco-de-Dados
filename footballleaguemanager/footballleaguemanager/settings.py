f49261ed802643dc20f8d7ff8849ff44ffbc630a
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2025-05-31 14:13:48 -0300

    fix: adjust some routes

--- footballleaguemanager/footballleaguemanager/settings.py
+++ footballleaguemanager/footballleaguemanager/settings.py
@@ -74,16 +74,16 @@ WSGI_APPLICATION = 'footballleaguemanager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': 'footballleaguemanager',
-#        'USER': 'adm',
-#        'PASSWORD': 'adm',
-#        'HOST': 'localhost',
-#        'PORT': '5432'
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'footballleaguemanager',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
 
 
 # Password validation
--- footballleaguemanager/footballleaguemanager/urls.py
+++ footballleaguemanager/footballleaguemanager/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#from django.contrib import admin
+from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('main.urls'))
-    #path('cadastros/', admin.site.urls),
-    #@path('bd/', admin.site.urls),
+    path('', include('main.urls')),
+    path('admin/', admin.site.urls)
 ]
